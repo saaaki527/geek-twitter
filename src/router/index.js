592915fe67f692c933235d1a8fef2cc00ab02e05
saaaki527 <saaaki527@gmail.com>
2021-03-02 16:41:01 +0900

    installed vue-router

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
+import About from "../views/About.vue"
+import MyPage from "../views/MyPage.vue"
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: About,
+  },
+  {
+    path: "/my-page",
+    name: "MyPage",
+    component: MyPage,
+  },
+]
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+})
+
+export default router
---
+++ src/views/MyPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="my-page">
+    <h1>This is my page</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
